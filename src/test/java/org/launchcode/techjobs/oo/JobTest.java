ebd53e68d0f914e653ad2816203bb11aef9e7f80
LynnG19 <antiphreeeze@gmail.com>
2023-11-01 16:57:24 -0500

    3rd

--- src/test/java/org/launchcode/techjobs/oo/JobTest.java
+++ src/test/java/org/launchcode/techjobs/oo/JobTest.java
@@ -1,5 +1,23 @@
 package org.launchcode.techjobs.oo;
 
+import org.junit.Test;
+import org.junit.Assert;
+
 public class JobTest {
-    //TODO: Create your unit tests here
+
+
+    @Test
+    public void testSettingJobId() {
+
+
+        Job aJobObj1 = new Job();
+        Job bJobObj2 = new Job();
+
+        assertNotEquals(bJobObj2.getId(), aJobObj1.getId());
+
+        //TODO: Create your unit tests here
+
+
+    }
+
 }
